55c4a587d4ef5cbcb075af341433f8d5db037d74
Matthew Greenspun <matthewgreenspun@gmail.com>
2021-05-13 13:39:10 -0400

    created tables for multiple choice and short answer questions

--- node-backend/sql/annoucements.sql
+++ node-backend/sql/annoucements.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE announcements(
   announcement_id UUID NOT NULL PRIMARY KEY,
-  user_id UUID NOT NULL REFERENCES user(id),
+  user_id UUID NOT NULL REFERENCES user(user_id),
   course_ids VARCHAR(100)[] NOT NULL,
   title VARCHAR(1000) NOT NULL,
   announcement_text TEXT NOT NULL,
--- node-backend/sql/short_answer_questions.sql
+++ node-backend/sql/short_answer_questions.sql
@@ -1,3 +1,16 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE short_answer_questions()
+CREATE TABLE short_answer_questions(
+  sa_question_id UUID NOT NULL PRIMARY KEY,
+  user_id UUID NOT NULL REFERENCES user(user_id);
+  course_ids VARCHAR(100)[] NOT NULL,
+  topic_id VARCHAR(100),
+  title VARCHAR(1000) NOT NULL,
+  description TEXT,
+  scheduled_time VARCHAR(5), -- hh:mm
+  posting_days VARCHAR(3)[] NOT NULL, -- ex: ['mon', 'wed', 'thu']
+  due_date - VARCHAR(10), -- yyyy-mm-dd or "DAY-POSTED
+  due_time - VARCHAR(5), -- hh:mm
+  submission_modification_mode VARCHAR(100),
+  max_points SMALLINT
+);
