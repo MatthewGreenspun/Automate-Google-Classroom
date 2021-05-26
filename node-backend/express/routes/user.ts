3fde45f895ad728fec0b1635dfb29950c4cc4211
Matthew Greenspun <matthewgreenspun@gmail.com>
2021-05-26 14:18:06 -0400

    created route to send back courses that the user has on google classroom

--- node-backend/express/routes/user.ts
+++ node-backend/express/routes/user.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import pool from "../../sql/Pool";
+import { google } from "googleapis";
 
 const router = express.Router();
+
 router.get("/me", (req, res) => {
   if (!req.user)
     res
@@ -12,6 +13,30 @@ router.get("/me", (req, res) => {
       displayName: (req.user as DatabaseUserData).display_name,
       profilePictureLink: (req.user as DatabaseUserData).profile_picture_link,
     });
+});
+
+router.get("/courses", async (req, res) => {
+  if (!req.user)
+    res
+      .status(401)
+      .send({ error: "you are unauthorized to make this request" });
+  else {
+    const oAuth2Client = new google.auth.OAuth2(
+      process.env.CLIENT_ID,
+      process.env.CLIENT_SECRET,
+      "http://localhost:8080/auth/googlecallback"
+    );
+
+    oAuth2Client.setCredentials({
+      refresh_token: (req.user as DatabaseUserData).refresh_token,
+    });
+    const classroom = google.classroom({ version: "v1", auth: oAuth2Client });
+    const {
+      data: { courses },
+    } = await classroom.courses.list({});
+
+    res.send(courses);
+  }
 });
 
 export default router;
