7a526f9efb53bd52d75452966a75580693a61590
Matthew Greenspun <matthewgreenspun@gmail.com>
2021-05-30 18:39:37 -0400

    added announcements table to the database

--- node-backend/sql/annoucements.sql
+++ node-backend/sql/annoucements.sql
@@ -2,10 +2,11 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE announcements(
   announcement_id UUID NOT NULL PRIMARY KEY,
-  user_id UUID NOT NULL REFERENCES user(user_id),
+  user_id VARCHAR(500) NOT NULL REFERENCES users(user_id),
   course_ids VARCHAR(100)[] NOT NULL,
   title VARCHAR(1000) NOT NULL,
   announcement_text TEXT NOT NULL,
   scheduled_time VARCHAR(5), -- hh:mm
-  posting_days VARCHAR(3)[] NOT NULL -- ex: ['mon', 'wed', 'thu']
+  posting_days VARCHAR(3)[] NOT NULL, -- ex: ['mon', 'wed', 'thu']
+  last_posted TIMESTAMP
 );
