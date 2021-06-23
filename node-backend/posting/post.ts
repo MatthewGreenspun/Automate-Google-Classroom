f2989cb98d5450c79b8c2968623f3125721d9fa7
Matthew Greenspun <matthewgreenspun@gmail.com>
2021-06-23 19:06:23 -0400

    added posting of multiple choice questions

--- node-backend/posting/post.ts
+++ node-backend/posting/post.ts
@@ -8,12 +8,12 @@ export async function postAll() {
     process.env.CLIENT_SECRET
   );
 
-  const { rows } = await pool.query<AnnouncementToPost>(`
-			SELECT announcements.*, users.refresh_token FROM announcements 
-			JOIN users ON announcements.user_id = users.user_id
-	 `);
+  const { rows: announcements } = await pool.query<AnnouncementToPost>(`
+  		SELECT announcements.*, users.refresh_token FROM announcements
+  		JOIN users ON announcements.user_id = users.user_id
+   `);
 
-  rows.forEach(async (announcement) => {
+  announcements.forEach(async (announcement) => {
     oAuth2Client.setCredentials({
       refresh_token: announcement.refresh_token,
     });
@@ -41,6 +41,71 @@ export async function postAll() {
             `error when creating announcement: ${
               announcement.title
             } for ${timeToISO(announcement.scheduled_time!)} \n${err} `
+          )
+        );
+    });
+  });
+
+  const { rows: mcQuestions } = await pool.query<McQuestionToPost>(`
+			SELECT multiple_choice_questions.*, users.refresh_token FROM multiple_choice_questions 
+			JOIN users ON multiple_choice_questions.user_id = users.user_id
+	 `);
+
+  mcQuestions.forEach(async (question) => {
+    oAuth2Client.setCredentials({
+      refresh_token: question.refresh_token,
+    });
+
+    const classroom = google.classroom({ version: "v1", auth: oAuth2Client });
+
+    const dueDateTime = question.due_time ? timeToISO(question.due_time) : null;
+
+    question.course_ids!.forEach((id) => {
+      classroom.courses.courseWork
+        .create({
+          courseId: id,
+          requestBody: {
+            title: question.title,
+            description: question.description,
+            workType: "MULTIPLE_CHOICE_QUESTION",
+            state: "DRAFT",
+            multipleChoiceQuestion: {
+              choices: question.choices,
+            },
+            scheduledTime: timeToISO(question.scheduled_time!),
+            dueDate: question.due_time
+              ? {
+                  day: Number(dueDateTime!.substring(8, 10)),
+                  month: Number(dueDateTime!.substring(5, 7)),
+                  year: Number(dueDateTime!.substring(0, 4)),
+                }
+              : undefined,
+            dueTime: question.due_time
+              ? {
+                  hours: Number(dueDateTime!.substring(11, 13)),
+                  minutes: Number(dueDateTime!.substring(14, 16)),
+                  seconds: 0,
+                  nanos: 0,
+                }
+              : undefined,
+            maxPoints: question.max_points,
+            submissionModificationMode: question.submission_modifiable
+              ? "MODIFIABLE"
+              : "MODIFIABLE_UNTIL_TURNED_IN",
+          },
+        })
+        .then(() =>
+          console.log(
+            `created multiple choice question: ${
+              question.title
+            } for ${timeToISO(question.scheduled_time!)}`
+          )
+        )
+        .catch((err) =>
+          console.error(
+            `error when creating multiple choice question: ${
+              question.title
+            } for ${timeToISO(question.scheduled_time!)} \n${err} `
           )
         );
     });
