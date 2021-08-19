f37804d2da6fa15a5af189199db01cc1aa1a47f5
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2021-08-19 18:53:19 -0400

    added unique constraint to user_id

--- node-backend/sql/users.sql
+++ node-backend/sql/users.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users(
-  user_id VARCHAR(500) NOT NULL,
+  user_id VARCHAR(500) NOT NULL UNIQUE,
   refresh_token VARCHAR(500) NOT NULL,
   display_name VARCHAR(1000),
   profile_picture_link VARCHAR(1000),
