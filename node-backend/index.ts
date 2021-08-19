ffc44571bef04070af4b721e2d7ebbdf24630359
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2021-08-19 14:10:07 -0400

    added way to run server without posting anything

--- node-backend/index.ts
+++ node-backend/index.ts
@@ -4,7 +4,9 @@ import { postAll } from "./posting/post";
 
 app.listen(process.env.PORT ?? 8080);
 
-schedule.scheduleJob("0 1 * * *", () => {
-  postAll();
-  console.log(`successfully posted at ${new Date()}`);
-});
+if (process.argv.includes("--post"))
+  schedule.scheduleJob("0 1 * * *", () => {
+    postAll();
+    console.log(`successfully posted at ${new Date()}`);
+  });
+else console.log("running server without posting any posts");
