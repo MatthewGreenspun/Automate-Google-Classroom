506d2855c3622dd43deaadd0d9ca46e857afb6b0
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2021-08-19 19:41:26 -0400

    logging for debugging purposes

--- dist/express/app.js
+++ dist/express/app.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv").config();
 var cors = require("cors");
@@ -17,29 +19,33 @@ var Pool_1 = __importDefault(require("../sql/Pool"));
 var path_1 = __importDefault(require("path"));
 require("./passportFunctions");
 var app = express_1.default();
-app.use(express_1.default.static(path_1.default.join(__dirname, "../", "build")));
+app.use(
+  express_1.default.static(path_1.default.join(__dirname, "../", "build"))
+);
 app.use(cors());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
-app.use(express_session_1.default({
+app.use(
+  express_session_1.default({
     store: new (require("connect-pg-simple")(express_session_1.default))({
-        pool: Pool_1.default,
+      pool: Pool_1.default,
     }),
     secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: { maxAge: 1000 * 60 * 60 * 6 }, // currently 6 hour, possibly change in prod
-}));
+  })
+);
 app.use(passport_1.default.initialize());
 app.use(passport_1.default.session());
 app.get("/", function (req, res) {
-    res.sendFile(path_1.default.join(__dirname, "../", "build", "index.html"));
+  res.sendFile(path_1.default.join(__dirname, "../", "build", "index.html"));
 });
 app.get("/posts", function (req, res) {
-    if (req.user)
-        res.sendFile(path_1.default.join(__dirname, "../", "build", "index.html"));
-    else
-        res.redirect("/auth/login");
+  if (req.user) {
+    console.log("req.user in posts route:", req.user);
+    res.sendFile(path_1.default.join(__dirname, "../", "build", "index.html"));
+  } else res.redirect("/auth/login");
 });
 app.use("/auth", auth_1.default);
 app.use("/api/v1/users", user_1.default);
