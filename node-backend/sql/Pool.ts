5407da50f365f33879efdfddbfd2dfad448a1579
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2021-08-24 17:23:58 -0400

    changed db to use conn string

--- node-backend/sql/Pool.ts
+++ node-backend/sql/Pool.ts
@@ -2,11 +2,7 @@ require("dotenv").config();
 import { Pool } from "pg";
 
 const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
